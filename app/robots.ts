8cff8ea0312f7c7ce09a2aad3ccfdd95aaa27496
luckybearcasino4 <anisimovzheka@inbox.ru>
2026-04-29 01:16:24 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next"
 
-const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://evacasino.app"
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://evacsino22.vercel.app"
 
 export default function robots(): MetadataRoute.Robots {
   return {
